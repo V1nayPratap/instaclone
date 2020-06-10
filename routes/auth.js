d7ea86ee10536261b5135e4f62d26b11c298b7ec
Vinay Pratap <pratap221098@gmail.com>
2020-06-10 22:01:36 +0530

    update try 1

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const User=mongoose.model("User")
 const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const {JWT_SECRET}=require('../config/key')
-const requireLogin=require('../middleWare/requireLogin')
+const requireLogin=require('../middleware/requireLogin')
 
 
 router.post('/signup',(req,res)=>{
